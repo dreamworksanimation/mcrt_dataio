0983302863848d6cf351952487e4640651f0ac0d
cegan <conrad.egan@dreamworks.com>
2023-06-27 14:50:58 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '12.2'
+    _version = '12.3'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -68,9 +68,9 @@ sconsTargets = {
 
 requires = [
     'arras4_core-4.10',
-    'mcrt_denoise-3.2',
+    'mcrt_denoise-3.3',
     'mcrt_messages-11.2',
-    'scene_rdl2-12.2'
+    'scene_rdl2-12.3'
 ]
 
 private_build_requires = [

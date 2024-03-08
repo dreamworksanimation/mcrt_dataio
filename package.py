35a5f8e6041f5d3e39c4ba4825c3b19a13bf85bc
Ron Woods <ron.woods@dreamworks.com>
2024-03-08 07:54:41 -0800

    MOONRAY-5137, MOONRAY-5212: remove scons build files.. (#138)

--- package.py
+++ package.py
@@ -32,13 +32,6 @@ authors = [
 help = ('For assistance, '
         "please contact the folio's owner at: moonbase-dev@dreamworks.com")
 
-if 'scons' in sys.argv:
-    build_system = 'scons'
-    build_system_pbr = 'bart_scons-10'
-else:
-    build_system = 'cmake'
-    build_system_pbr = 'cmake_modules'
-
 variants = [
     ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2021.0', 'gcc-9.3.x.1'],
     ['os-CentOS-7', 'opt_level-debug', 'refplat-vfx2021.0', 'gcc-9.3.x.1'],
@@ -65,14 +58,6 @@ variants = [
 conf_rats_variants = variants[0:2]
 conf_CI_variants = list(filter(lambda v: 'os-CentOS-7' in v, variants))
 
-scons_targets = ['@install'] + unittestflags
-sconsTargets = {
-    'refplat-vfx2020.3': scons_targets,
-    'refplat-vfx2021.0': scons_targets,
-    'refplat-vfx2022.0': scons_targets,
-    'refplat-vfx2023.0': scons_targets,
-}
-
 requires = [
     'arras4_core-4.10',
     'freetype-2',
@@ -82,7 +67,7 @@ requires = [
 ]
 
 private_build_requires = [
-    build_system_pbr,
+    'cmake_modules',
     'cppunit',
 ]
 
@@ -93,15 +78,10 @@ testentry = lambda i: ("variant%d" % i,
 testlist = [testentry(i) for i in range(len(variants))]
 tests = dict(testlist)
 
-if build_system is 'cmake':
-    def commands():
-        prependenv('CMAKE_MODULE_PATH', '{root}/lib64/cmake')
-        prependenv('LD_LIBRARY_PATH', '{root}/lib64')
-        prependenv('PATH', '{root}/bin')
-else:
-    def commands():
-        prependenv('LD_LIBRARY_PATH', '{root}/lib')
-        prependenv('PATH', '{root}/bin')
+def commands():
+    prependenv('CMAKE_MODULE_PATH', '{root}/lib64/cmake')
+    prependenv('LD_LIBRARY_PATH', '{root}/lib64')
+    prependenv('PATH', '{root}/bin')
 
 uuid = '2e6694ba-3c4d-4d2d-bc67-e5b031886c38'
 

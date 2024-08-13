f2f197df78e3d637b337eff867bbab625345d556
mrday <mike.day@dreamworks.com>
2024-08-13 14:08:46 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -17,7 +17,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '14.11'
+    _version = '14.12'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -58,9 +58,9 @@ conf_CI_variants = list(filter(lambda v: 'os-CentOS-7' in v, variants))
 requires = [
     'arras4_core-4.10',
     'freetype-2',
-    'mcrt_denoise-5.11',
+    'mcrt_denoise-5.12',
     'mcrt_messages-13.3',
-    'scene_rdl2-14.11'
+    'scene_rdl2-14.12'
 ]
 
 private_build_requires = [

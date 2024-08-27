33a1fe8fbec4a4df6cf6b2dbb9a7f05c6059ddc6
alee1 <ashley.lee@dreamworks.com>
2024-08-27 14:25:32 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -17,7 +17,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '14.12'
+    _version = '14.13'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -58,9 +58,9 @@ conf_CI_variants = list(filter(lambda v: 'os-CentOS-7' in v, variants))
 requires = [
     'arras4_core-4.10',
     'freetype-2',
-    'mcrt_denoise-5.12',
+    'mcrt_denoise-5.13',
     'mcrt_messages-13.3',
-    'scene_rdl2-14.12'
+    'scene_rdl2-14.13'
 ]
 
 private_build_requires = [
